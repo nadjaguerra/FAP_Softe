5b3b789a3a2e41a4c2340b3334ebadf00844dcf2
Nadja Guerra <112482228+nadjaguerra@users.noreply.github.com>
2023-08-21 10:46:52 -0300

    fatorial.js

--- fatorial.js
+++ fatorial.js
@@ -3,10 +3,7 @@ function fatorial(n) {
     if (n <= 1) {
         return 1;
     }
-    else{
         return n * fatorial(n - 1);
-
-    }
 }
 n = parseInt(prompt("informe n: "))
 alert(fatorial(n))
