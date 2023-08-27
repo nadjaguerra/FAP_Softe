f0fb98311430137ae65cf98f872b590305ee93df
Nadja Guerra <112482228+nadjaguerra@users.noreply.github.com>
2023-08-27 16:19:41 -0300

    media_notaminima.js

--- Media_modulo2/media_notaminima.js
+++ Media_modulo2/media_notaminima.js
@@ -1,15 +1,15 @@
 function calcMedia() {
     let soma = 0;
     for (i = 0; i <= 1; i++) {
-        nota = parseFloat(prompt(`Informe a ${i + 1}ª nota: `));
-        soma += nota;
+        nota = parseFloat(prompt(`Informe a ${i + 1}ª nota: `))
+        soma += nota
     }
-    return 21 - soma;               //Considerando que a média para ser aprovado é 7
+    return 21 - soma               //Considerando que a média para ser aprovado é 7
 }
 
 //Sendo 10 a nota máxima de uma prova
-let notaMin = calcMedia();
+let notaMin = calcMedia()
 let result = notaMin > 10 ? `Não é possível atingir a média com a terceira nota. Seria necessário ${notaMin} pontos.` : 
-`Para ser aprovado, a terceira nota precisa ser maior ou igual a ${notaMin.toFixed(2)}.`;
+`Para ser aprovado, a terceira nota precisa ser maior ou igual a ${notaMin.toFixed(2)}.`
 
-console.log(result);
+console.log(result)
