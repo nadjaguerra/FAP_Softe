7a601f9cd65b7095af7f7ede16b4f0c815d3af63
Nadja Guerra <112482228+nadjaguerra@users.noreply.github.com>
2023-08-28 17:25:59 -0300

    media_operadorT.js

--- Media_modulo2/media_operadorT.js
+++ Media_modulo2/media_operadorT.js
@@ -1,10 +1,20 @@
-function calcMedia(){
-    let soma = 0
-    for(i=0;i<=2;i++) {
-        nota = parseFloat(prompt(`Informe a ${i+1}ª nota: `))
-        soma += nota
+//Calcule a média e informe se está aprovado ou não. Utilize pelo menos um operador ternário.
+
+function calcMedia() {
+    let soma = 0;
+    for (let i = 0; i <= 2; i++) {
+        try {
+            let nota = parseFloat(prompt(`Informe a ${i + 1}ª nota: `));
+            if (isNaN(nota)) {
+                throw new Error('Informe apenas números.');
+            }
+            soma += nota;
+        } catch (error) {
+            console.error(error.message);
+            i--;                     // repete entrada da nota inválida.
+        }
     }
-    return soma/3           
+    return soma / 3;
 }
 
 let media = calcMedia();
