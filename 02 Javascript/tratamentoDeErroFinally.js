968160609093953235f44d9493ecb1177806ff96
Nadja Guerra <112482228+nadjaguerra@users.noreply.github.com>
2023-10-08 18:21:02 -0300

    tratamentoDeErroFinally.js
    Tratamento de Erro

---
+++ 02 Javascript/tratamentoDeErroFinally.js
@@ -0,0 +1,33 @@
+const calculadoraMedia = {
+  notas: [],
+  excecaoOcorreu: false,
+  calcularMedia: function() {
+    let soma = 0;
+
+    for (let i = 0; i <= 2; i++) {
+      try {
+        let nota = parseFloat(prompt(`Informe a ${i + 1}ª nota: `));
+        if (isNaN(nota)) {
+          throw new Error('Informe apenas números.');
+        }
+        soma += nota;
+      } catch (error) {
+        console.error(error.message);
+        i--;
+        this.excecaoOcorreu = true;
+      } finally {
+        if (!this.excecaoOcorreu) {
+          console.log(`
+          Nota adicionada.
+          `);
+        }
+      }
+    }
+
+    return soma / 3;
+  }
+};
+
+const mediaCalculada = calculadoraMedia.calcularMedia();
+console.log(`------------------------
+A média do aluno é ${mediaCalculada.toFixed(2)}`);
